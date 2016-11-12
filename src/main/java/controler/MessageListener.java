a7b583ec7cddc2b6c028f508ecb41cb0418bc94b
Steven Pescheteau <steven.pescheteau@gmail.com>
2016-11-12 17:08:13 +0100

    Message Listeners have been corrected because of precedents commits : if user name change, it will be updated into database. Same thing for guild name.

--- src/main/java/controler/MessageListener.java
+++ src/main/java/controler/MessageListener.java
@@ -3,6 +3,8 @@ package controler;
 import commands.*;
 import data.Guild;
 import data.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import sx.blah.discord.api.events.EventSubscriber;
 import sx.blah.discord.handle.impl.events.MessageReceivedEvent;
 
@@ -11,6 +13,8 @@ import sx.blah.discord.handle.impl.events.MessageReceivedEvent;
  */
 public class MessageListener {
 
+    private final static Logger LOG = LoggerFactory.getLogger(MessageListener.class);
+
     public MessageListener(){
         super();
     }
@@ -18,11 +22,26 @@ public class MessageListener {
         public void onReady(MessageReceivedEvent event) {
             // If the author doesn't exist in our database, we added it with the lowest rights
             String authorId = event.getMessage().getAuthor().getID();
+            String authorName = event.getMessage().getAuthor().getName();
             String guildId = event.getMessage().getGuild().getID();
-            if (! Guild.getGuild().containsKey(guildId))
-                new Guild(guildId).addToDatabase();
-            if (! User.getUsers().get(guildId).containsKey(authorId))
-                new User(authorId, User.RIGHT_INVITE, Guild.getGuild().get(guildId)).addToDatabase();
+            String guildName = event.getMessage().getGuild().getName();
+
+            if (! Guild.getGuild().containsKey(guildId)) {
+                LOG.info("Guild " + guildId + " - " + guildName + " is added to database.");
+                new Guild(guildId, guildName).addToDatabase();
+            }
+            else if (! Guild.getGuild().get(guildId).getName().equals(guildName))
+                // GuildName from database is deprecated : it have to be updated.
+                Guild.getGuild().get(guildId).setName(guildName);
+
+            if (! User.getUsers().get(guildId).containsKey(authorId)) {
+                LOG.info("User " + authorId + " - " + authorName + " is added to database.");
+                new User(authorId, authorName, User.RIGHT_INVITE, Guild.getGuild().get(guildId)).addToDatabase();
+            }
+            else if (! User.getUsers().get(guildId).get(authorId).getName().equals(authorName))
+                // AuthorName from database is deprecated : it have to be updated.
+                User.getUsers().get(guildId).get(authorId).setName(authorName);
+
 
             for(Command command : Command.commands)
                 command.request(event.getMessage());
