2ffe74055e475d4a35e48ee614b882098c6bd73b
Steven Pescheteau <steven.pescheteau@gmail.com>
2016-11-14 09:36:47 +0100

    Item command can throw discord exception and call Message.send.

--- src/main/java/commands/ItemCommand.java
+++ src/main/java/commands/ItemCommand.java
@@ -1,15 +1,11 @@
 package commands;
 
-import data.ClientConfig;
 import data.Constants;
 import data.ItemDofus;
+import exceptions.InDeveloppmentException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import sx.blah.discord.handle.obj.IMessage;
-import sx.blah.discord.util.DiscordException;
-import sx.blah.discord.util.MessageBuilder;
-import sx.blah.discord.util.MissingPermissionsException;
-import sx.blah.discord.util.RequestBuffer;
 
 import java.util.List;
 import java.util.regex.Pattern;
@@ -31,22 +27,10 @@ public class ItemCommand extends AbstractCommand{
     @Override
     public boolean request(IMessage message) {
         if (super.request(message)){
+
             //TODO
-            RequestBuffer.request(() -> {
-                try {
-                    new MessageBuilder(ClientConfig.CLIENT())
-                            .withChannel(message.getChannel())
-                            .withContent("Cette commande est en cours de développement "
-                                    + message.getAuthor()
-                                    + ", tu pourrais attendre un peu quand même !")
-                            .build();
-                } catch (DiscordException e){
-                    LOG.error(e.getErrorMessage());
-                } catch(MissingPermissionsException e){
-                    LOG.warn(Constants.name + " n'a pas les permissions pour appliquer cette requête.");
-                }
-                return null;
-            });
+
+            new InDeveloppmentException().throwException(message, this);
             return true;
         }
 
