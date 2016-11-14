57e4204a8149c3564193fbf3767a3bd316a4bf58
Steven Pescheteau <steven.pescheteau@gmail.com>
2016-11-14 09:32:15 +0100

    Exception when almanax cannot be found (official website down for example).

---
+++ src/main/java/exceptions/AlmanaxNotFoundException.java
@@ -0,0 +1,20 @@
+package exceptions;
+
+import commands.Command;
+import discord.Message;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import sx.blah.discord.handle.obj.IMessage;
+
+/**
+ * Created by steve on 14/11/2016.
+ */
+public class AlmanaxNotFoundException implements Exception {
+
+    private final static Logger LOG = LoggerFactory.getLogger(AlmanaxNotFoundException.class);
+
+    @Override
+    public void throwException(IMessage message, Command command) {
+        Message.send(message.getChannel(), "Impossible de trouver des informations sur l'almanax désiré.");
+    }
+}
