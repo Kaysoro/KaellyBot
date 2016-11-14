774047c8bbcb29549605a7b4736cda61a21e97c4
Steven Pescheteau <steven.pescheteau@gmail.com>
2016-11-14 09:19:15 +0100

    Message class permit to send message via discord. All request buffer have to use this class.

---
+++ src/main/java/discord/Message.java
@@ -0,0 +1,32 @@
+package discord;
+
+import data.ClientConfig;
+import data.Constants;
+import org.slf4j.LoggerFactory;
+import sx.blah.discord.handle.obj.IChannel;
+import sx.blah.discord.util.DiscordException;
+import sx.blah.discord.util.MessageBuilder;
+import sx.blah.discord.util.MissingPermissionsException;
+import sx.blah.discord.util.RequestBuffer;
+
+/**
+ * Created by steve on 14/11/2016.
+ */
+public class Message {
+
+    public static void send(IChannel channel, String content){
+        RequestBuffer.request(() -> {
+            try {
+                new MessageBuilder(ClientConfig.CLIENT())
+                        .withChannel(channel)
+                        .withContent(content)
+                        .build();
+            } catch (DiscordException e){
+                LoggerFactory.getLogger(Message.class).error(e.getErrorMessage());
+            } catch(MissingPermissionsException e){
+                LoggerFactory.getLogger(Message.class).warn(Constants.name + " n'a pas les permissions pour appliquer cette requête.");
+            }
+            return null;
+        });
+    }
+}
