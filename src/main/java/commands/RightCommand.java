3d90a15444f4f508f28d7b1ebcca4dc2d12b0e14
Steven Pescheteau <steven.pescheteau@gmail.com>
2016-11-14 23:47:50 +0100

    The whole part of RightCommand is ready. /!\ Have to be tested.

--- src/main/java/commands/RightCommand.java
+++ src/main/java/commands/RightCommand.java
@@ -1,17 +1,16 @@
 package commands;
 
-import data.ClientConfig;
 import data.Constants;
 import data.User;
+import discord.Message;
 import exceptions.AutoChangeRightsException;
+import exceptions.NoRightsForRolesException;
 import exceptions.NotEnoughRightsException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import sx.blah.discord.handle.obj.IMessage;
-import sx.blah.discord.util.DiscordException;
-import sx.blah.discord.util.MessageBuilder;
-import sx.blah.discord.util.MissingPermissionsException;
-import sx.blah.discord.util.RequestBuffer;
+import sx.blah.discord.handle.obj.IRole;
+import sx.blah.discord.handle.obj.IUser;
 
 import java.util.regex.Pattern;
 
@@ -31,36 +30,75 @@ public class RightCommand extends AbstractCommand{
     public boolean request(IMessage message) {
         if (super.request(message)) {
 
+            User author = User.getUsers().get(message.getGuild().getID()).get(message.getAuthor().getID());
+
+
             if (m.group(3) != null) { // Level precised : editing
 
-                if (User.getUsers().get(message.getGuild().getID())
-                        .get(message.getAuthor().getID()).getRights() >= User.RIGHT_MODERATOR) {
+                String idDecorated = m.group(2).replaceAll("\\s", "");
+                String id = idDecorated.replaceAll("\\W", "");
+                int level = Integer.parseInt(m.group(3).replaceAll("\\s", ""));
 
-                    String id = m.group(2).replaceAll("\\s", "");
-                    String level = m.group(3).replaceAll("\\s", "");
+                if (author.getRights() >= User.RIGHT_MODERATOR) {
+                    if (idDecorated.contains("&")){ // Manage Groups
+                        IRole role = message.getGuild().getRoleByID(id);
 
-                    try {
-                        if (!m.group(2).equals(message.getAuthor().getID()))
-                            User.getUsers().get(message.getGuild().getID())
-                                    .get(id).changeRight(Integer.parseInt(level));
-                        else {
-                            new AutoChangeRightsException().throwException(message, this);
+                        for(IUser user : message.getGuild().getUsers()){
+
+                            User target = User.getUsers().get(message.getGuild().getID()).get(user.getID());
+
+                            if (user.getRolesForGuild(message.getGuild()).contains(role)){
+                                if ((! target.getId().equals(author.getId())) &&
+                                    author.getRights() >= target.getRights())
+                                    target.changeRight(level);
+                                else
+                                    Message.send(message.getChannel(), "Les droits d'administration de "
+                                    + target.getName() + " n'ont pu être changés.\n");
+                            }
                         }
-                    } catch (NullPointerException e) {
-                        //TODO add user
-                        LOG.warn("L'utilisateur <@!" + id + "> n'existe pas.");
                     }
-                } else {
+                    else if (idDecorated.contains("!")){ // Manage users
+                        User target = User.getUsers().get(message.getGuild().getID()).get(id);
+
+                        if (id.equals(message.getAuthor().getID())) {
+                            new AutoChangeRightsException().throwException(message, this);
+                            return false;
+                        }
+
+                        if (author.getRights() < target.getRights()) {
+                            new NotEnoughRightsException().throwException(message, this);
+                            return false;
+                        }
+
+                        target.changeRight(level);
+                    }
+                } // Not an admin or a moderator
+                else {
                     new NotEnoughRightsException().throwException(message, this);
                     return false;
                 }
-                return true;
             } else { // Level is not precised : consulting
 
+                if (m.group(2) != null){ // Author want to know specific user's rights.
+                    String idDecorated = m.group(2).replaceAll("\\s", "");
+                    String id = idDecorated.replaceAll("\\W", "");
+
+                    if (idDecorated.contains("!")){
+                        User target = User.getUsers().get(message.getGuild().getID()).get(id);
+                        Message.send(message.getChannel(), target.getName()
+                                + " a des droits d'administration de niveau "
+                                + target.getRights() + ".");
+                    }
+                    else
+                        new NoRightsForRolesException().throwException(message, this);
+                }
+                else
+                    Message.send(message.getChannel(), "Tu as les droits d'administration de niveau "
+                            + author.getRights() + ".");
             }
         }
 
-        return false;
+        return true;
     }
 
     @Override
