7528763c753d2d997c54787a6c532bd6f5782ca3
Steven Pescheteau <steven.pescheteau@gmail.com>
2016-11-14 09:20:11 +0100

    Exception permit to throw generic message for the user who call the command.

---
+++ src/main/java/exceptions/Exception.java
@@ -0,0 +1,12 @@
+package exceptions;
+
+import commands.Command;
+import sx.blah.discord.handle.obj.IMessage;
+
+/**
+ * Created by steve on 14/11/2016.
+ */
+public interface Exception {
+
+    public void throwException(IMessage message, Command command);
+}
