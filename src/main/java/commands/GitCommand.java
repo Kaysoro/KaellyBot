4be6c5b3d85d9ab4e340218ad98d77e932c4ec13
Steven Pescheteau <steven.pescheteau@gmail.com>
2016-11-14 09:35:50 +0100

    Git command can throw discord exception and call Message.send.

--- src/main/java/commands/GitCommand.java
+++ src/main/java/commands/GitCommand.java
@@ -1,14 +1,10 @@
 package commands;
 
-import data.ClientConfig;
 import data.Constants;
+import discord.Message;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import sx.blah.discord.handle.obj.IMessage;
-import sx.blah.discord.util.DiscordException;
-import sx.blah.discord.util.MessageBuilder;
-import sx.blah.discord.util.MissingPermissionsException;
-import sx.blah.discord.util.RequestBuffer;
 
 import java.util.regex.Pattern;
 
@@ -27,20 +23,9 @@ public class GitCommand extends AbstractCommand{
     @Override
     public boolean request(IMessage message) {
         if (super.request(message)){
-            RequestBuffer.request(() -> {
-                try {
-                    new MessageBuilder(ClientConfig.CLIENT())
-                            .withChannel(message.getChannel())
-                            .withContent("Le code source de " + Constants.name + " se trouve sur " + Constants.git
-                                    + " ! N'hésitez pas à réagir à ce propos :)")
-                            .build();
-                } catch (DiscordException e){
-                    LOG.error(e.getErrorMessage());
-                } catch(MissingPermissionsException e){
-                    LOG.warn(Constants.name + " n'a pas les permissions pour appliquer cette requête.");
-                }
-                return null;
-            });
+
+            Message.send(message.getChannel(), "Le code source de " + Constants.name + " se trouve sur " + Constants.git
+                    + " ! N'hésitez pas à réagir à ce propos :)");
             return true;
         }
 
