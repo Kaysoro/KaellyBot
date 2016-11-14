ce77caebc70371cc483a7bb1adc518e1e57806f5
Steven Pescheteau <steven.pescheteau@gmail.com>
2016-11-14 09:37:57 +0100

    Map command can throw discord exception and call Message.send.

--- src/main/java/commands/MapCommand.java
+++ src/main/java/commands/MapCommand.java
@@ -1,15 +1,11 @@
 package commands;
 
-import data.ClientConfig;
 import data.Constants;
+import discord.Message;
+import exceptions.BadUseCommandException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import sx.blah.discord.handle.obj.IMessage;
-import sx.blah.discord.util.DiscordException;
-import sx.blah.discord.util.MessageBuilder;
-import sx.blah.discord.util.MissingPermissionsException;
-import sx.blah.discord.util.RequestBuffer;
-
 import java.util.Random;
 import java.util.regex.Pattern;
 
@@ -36,37 +32,12 @@ public class MapCommand extends AbstractCommand{
                     maps = new String[]{"I", "II", "III", "IV", "V", "VI",
                             "VII", "VIII", "IX", "X", "XI", "XII"};
 
-                RequestBuffer.request(() -> {
-                    try {
-                        new MessageBuilder(ClientConfig.CLIENT())
-                                .withChannel(message.getChannel())
-                                .withContent(message.getAuthor() + ", le combat aura lieu sur la carte "
-                                + maps[new Random().nextInt(maps.length)] + " !")
-                                .build();
-                    } catch (DiscordException e){
-                        LOG.error(e.getErrorMessage());
-                    } catch(MissingPermissionsException e){
-                        LOG.warn(Constants.name + " n'a pas les permissions pour appliquer cette requête.");
-                    }
-                    return null;
-                });
+                Message.send(message.getChannel(), message.getAuthor() + ", le combat aura lieu sur la carte "
+                        + maps[new Random().nextInt(maps.length)] + " !");
 
             } catch(Exception e ){
-                e.printStackTrace();
-                LOG.warn("Malformation des arguments de la requête");
-                RequestBuffer.request(() -> {
-                    try {
-                        new MessageBuilder(ClientConfig.CLIENT()).withChannel(message.getChannel())
-                                .withContent(message.getAuthor() + ", !map ne s'utilise pas comme ça !"
-                                        + "\nVoici un exemple : `!map 1 2 3`").build();
-
-                    } catch (DiscordException e2){
-                        LOG.error(e2.getErrorMessage());
-                    } catch(MissingPermissionsException e2){
-                        LOG.warn(Constants.name + " n'a pas les permissions pour appliquer cette requête.");
-                    }
-                    return null;
-                });
+                LOG.warn("Malformation des arguments de la requête : " + e.getMessage());
+                new BadUseCommandException().throwException(message, this);
             }
             return true;
         }
