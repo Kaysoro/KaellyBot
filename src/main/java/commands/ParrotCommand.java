57fd1f8efb9447f7e1cc227ff877332541f2ef2a
Steven Pescheteau <steven.pescheteau@gmail.com>
2016-11-14 09:38:08 +0100

    Parrot command can throw discord exception and call Message.send.

--- src/main/java/commands/ParrotCommand.java
+++ src/main/java/commands/ParrotCommand.java
@@ -1,14 +1,10 @@
 package commands;
 
-import data.ClientConfig;
 import data.Constants;
+import discord.Message;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import sx.blah.discord.handle.obj.IMessage;
-import sx.blah.discord.util.DiscordException;
-import sx.blah.discord.util.MessageBuilder;
-import sx.blah.discord.util.MissingPermissionsException;
-import sx.blah.discord.util.RequestBuffer;
 
 import java.util.regex.Pattern;
 
@@ -27,19 +23,7 @@ public class ParrotCommand extends AbstractCommand{
     @Override
     public boolean request(IMessage message) {
         if (super.request(message)) {
-            RequestBuffer.request(() -> {
-                try {
-                    new MessageBuilder(ClientConfig.CLIENT())
-                            .withChannel(message.getChannel())
-                            .withContent(m.group(2))
-                            .build();
-                } catch (DiscordException e){
-                    LOG.error(e.getErrorMessage());
-                } catch(MissingPermissionsException e){
-                    LOG.warn(Constants.name + " n'a pas les permissions pour appliquer cette requête.");
-                }
-                return null;
-            });
+            Message.send(message.getChannel(), m.group(2));
             return true;
         }
         return false;
