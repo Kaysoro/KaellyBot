ef672178fcab8fbc8a45c899b7b19aa3a2d41c1f
Steven Pescheteau <steven.pescheteau@gmail.com>
2016-11-29 20:13:27 +0100

    Job can be found by users. Optimisation done on database, all jobs per user are not created but only the strict minimum rows exiged.

--- src/main/java/data/Job.java
+++ src/main/java/data/Job.java
@@ -3,6 +3,10 @@ package data;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -14,28 +18,101 @@ import java.util.Map;
 public class Job {
 
     private final static Logger LOG = LoggerFactory.getLogger(Job.class);
-    private static List<Job> jobs;
+    private static List<String> jobs;
     private String name;
+    private int level;
+    private User user;
 
-    private Job(String name){
+    public Job(String name, int level, User user){
         this.name = name;
+        this.level = level;
+        this.user = user;
     }
 
-    public Map<User, Integer> getJobbers(){
-        Map<User, Integer> jobbers = new HashMap<User, Integer>();
+    public void setLevel(int level){
+        this.level = level;
 
-        //TODO SQL Part
+        Connexion connexion = Connexion.getInstance();
+        Connection connection = connexion.getConnection();
 
-        return jobbers;
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                    "UPDATE Job_User SET level = ?"
+                            + "WHERE name_job = ? AND id_user = ?;");
+            preparedStatement.setInt(1, level);
+            preparedStatement.setString(2, name);
+            preparedStatement.setString(3, user.getId());
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            LOG.error(e.getMessage());
+        }
     }
 
-    public static List<Job> getJobs(){
+    public void addToDatabase(){
+        if (! user.getJobs().containsKey(name)) {
+            user.getJobs().put(name, this);
+            Connexion connexion = Connexion.getInstance();
+            Connection connection = connexion.getConnection();
+
+            try {
+                PreparedStatement preparedStatement = connection.prepareStatement(
+                        "INSERT INTO Job_User(name_job, id_user, level) VALUES(?, ?, ?);");
+                preparedStatement.setString(1, name);
+                preparedStatement.setString(2, user.getId());
+                preparedStatement.setInt(3, level);
+
+                preparedStatement.executeUpdate();
+            } catch (SQLException e) {
+                LOG.error(e.getMessage());
+            }
+        }
+    }
+
+    public static Map<String, Job> getJobs(User user){
+        Map<String, Job> jobs = new HashMap<String, Job>();
+
+        Connexion connexion = Connexion.getInstance();
+        Connection connection = connexion.getConnection();
+
+        String name;
+        int level;
+
+        try {
+            PreparedStatement query = connection.prepareStatement("SELECT name, level"
+                    + " FROM Job_User WHERE id_user = ?;");
+            query.setString(1, user.getId());
+
+            ResultSet resultSet = query.executeQuery();
+
+            while (resultSet.next()) {
+                name = resultSet.getString("name");
+                level = resultSet.getInt("level");
+                jobs.put(name, new Job(name, level, user));
+            }
+        } catch (SQLException e) {
+            LOG.error(e.getMessage());
+        }
+
+        return jobs;
+    }
+
+    public static List<String> getJobs(){
         if (jobs == null){
-            jobs = new ArrayList<Job>();
-            jobs.add(new Job("Forgeron"));
-            jobs.add(new Job("Tailleur"));
-            jobs.add(new Job("Sculpteur"));
-            //TODO gather data from databsse
+            jobs = new ArrayList<String>();
+
+            Connexion connexion = Connexion.getInstance();
+            Connection connection = connexion.getConnection();
+
+            try {
+                PreparedStatement query = connection.prepareStatement("SELECT name FROM Job");
+                ResultSet resultSet = query.executeQuery();
+
+                while (resultSet.next())
+                    jobs.add(resultSet.getString("name"));
+            } catch (SQLException e) {
+                LOG.error(e.getMessage());
+            }
         }
         return jobs;
     }
