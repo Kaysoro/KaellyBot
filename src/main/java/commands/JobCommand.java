4e0278a49ef16f6d0a77d5e5709378f155175428
Steven Pescheteau <steven.pescheteau@gmail.com>
2016-11-29 21:05:31 +0100

    JobCommand permit to edit your jobs level. Consultation is still not ready.

--- src/main/java/commands/JobCommand.java
+++ src/main/java/commands/JobCommand.java
@@ -1,10 +1,20 @@
 package commands;
 
 import data.Constants;
+import data.Guild;
+import data.Job;
+import data.User;
+import discord.Message;
 import exceptions.InDeveloppmentException;
+import exceptions.JobNotFoundException;
+import exceptions.TooMuchPossibilitiesException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import sx.blah.discord.handle.obj.IMessage;
+
+import java.text.Normalizer;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Pattern;
 
 /**
@@ -23,13 +33,53 @@ public class JobCommand extends AbstractCommand{
     public boolean request(IMessage message) {
         if (super.request(message)){
 
-            //TODO
+            List<String> jobs = getJob(m.group(2));
 
-            new InDeveloppmentException().throwException(message, this);
-            return true;
+            if (jobs.size() == 1) {
+                if (m.group(3) != null) { // setting data
+                    User author = User.getUsers().get(message.getGuild().getID())
+                            .get(message.getAuthor().getID());
+                    int level = Integer.parseInt(m.group(3));
+                    if (! author.getJobs().containsKey(jobs.get(0)))
+                        new Job(jobs.get(0), level, author).addToDatabase();
+                    else
+                        author.getJobs().get(jobs.get(0)).setLevel(level);
+
+                    if (author.getJob(jobs.get(0)) > 0)
+                        Message.send(message.getChannel(), author.getName()
+                                + " est inscrit dans l'annuaire en tant que " + jobs.get(0)
+                                + " niv. " + author.getJob(jobs.get(0)) + ".");
+                    else
+                        Message.send(message.getChannel(), author.getName()
+                                + " n'est plus inscrit dans l'annuaire en tant que " + jobs.get(0) + ".");
+                }
+                else { // Consultation
+                    //TODO
+                }
+
+                Message.send(message.getChannel(), jobs.get(0).toString());
+            }
+            else if(jobs.size() > 1)
+                new TooMuchPossibilitiesException().throwException(message, this);
+            else
+                new JobNotFoundException().throwException(message, this);
         }
 
         return false;
+    }
+
+    public List<String> getJob(String nameProposed){
+        nameProposed = Normalizer.normalize(nameProposed, Normalizer.Form.NFD)
+                .replaceAll("\\p{InCombiningDiacriticalMarks}+", "").toLowerCase();
+        nameProposed = nameProposed.replaceAll("\\W+", "");
+        List<String> jobs = new ArrayList<String>();
+
+        for(String job : Job.getJobs())
+            if (Normalizer.normalize(job, Normalizer.Form.NFD)
+                    .replaceAll("\\p{InCombiningDiacriticalMarks}+", "")
+                    .toLowerCase().startsWith(nameProposed))
+                jobs.add(job);
+        return jobs;
     }
 
     @Override
@@ -41,6 +91,7 @@ public class JobCommand extends AbstractCommand{
     public String helpDetailed() {
         return help()
                 + "\n`" + Constants.prefixCommand + "job` : renvoit l'annuaire des artisans pour ce métier."
-                + "\n`" + Constants.prefixCommand + "job `*`niveau`* : vous ajoute à l'annuaire du métier correspondant\n";
+                + "\n`" + Constants.prefixCommand + "job `*`niveau`* : vous ajoute à l'annuaire du métier correspondant." +
+                "Si vous indiquez 0, vous êtes supprimé de l'annuaire pour ce métier.\n";
     }
 }
