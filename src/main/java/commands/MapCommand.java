ac86d9c9c28ad651c6c738105d62a1a31678f034
Steven Pescheteau <steven.pescheteau@gmail.com>
2017-08-14 22:26:14 +0200

    !map contains the following maps : XIII, XIV, XV, XVI, XVII

--- src/main/java/commands/MapCommand.java
+++ src/main/java/commands/MapCommand.java
@@ -30,14 +30,22 @@ public class MapCommand extends AbstractCommand{
         urls.put("X", "https://image.noelshack.com/fichiers/2017/14/1491433379-x.png");
         urls.put("XI", "https://image.noelshack.com/fichiers/2017/14/1491433381-xi.png");
         urls.put("XII", "https://image.noelshack.com/fichiers/2017/14/1491433381-xii.png");
+        urls.put("XIII", "https://image.noelshack.com/fichiers/2017/33/1/1502741729-xiii.png");
+        urls.put("XIV", "https://image.noelshack.com/fichiers/2017/33/1/1502741732-xiv.png");
+        urls.put("XV", "https://image.noelshack.com/fichiers/2017/33/1/1502741732-xv.png");
+        urls.put("XVI", "https://image.noelshack.com/fichiers/2017/33/1/1502741731-xvi.png");
+        urls.put("XVII", "https://image.noelshack.com/fichiers/2017/33/1/1502741730-xvii.png");
     }
 
     public MapCommand(){
         super("map",
-        "((\\s+I|\\s+II|\\s+III|\\s+IV|\\s+V|\\s+VI|\\s+VII|\\s+VIII|\\s+IX|\\s+X|\\s+XI|\\s+XII"
-              + "|\\s+i|\\s+ii|\\s+iii|\\s+iv|\\s+v|\\s+vi|\\s+vii|\\s+viii|\\s+ix|\\s+x|\\s+xi|\\s+xii"
-              + "|\\s+1|\\s+2|\\s+3|\\s+4|\\s+5|\\s+6|\\s+7|\\s+8|\\s+9|\\s+10|\\s+11|\\s+12"
-              + ")+)?");
+        "((\\s+I|\\s+II|\\s+III|\\s+IV|\\s+V|\\s+VI|\\s+VII|\\s+VIII|\\s+IX|\\s+X"
+                + "|\\s+XI|\\s+XII|\\s+XIII|\\s+XIV|\\s+XV|\\s+XVI|\\s+XVII"
+                + "|\\s+i|\\s+ii|\\s+iii|\\s+iv|\\s+v|\\s+vi|\\s+vii|\\s+viii|\\s+ix|\\s+x"
+                + "|\\s+xi|\\s+xii|\\s+xiii|\\s+xiv|\\s+xv|\\s+xvi|\\s+xvii"
+                + "|\\s+1|\\s+2|\\s+3|\\s+4|\\s+5|\\s+6|\\s+7|\\s+8|\\s+9|\\s+10"
+                + "|\\s+11|\\s+12|\\s+13|\\s+14|\\s+15|\\s+16|\\s+17"
+                + ")+)?");
     }
 
     @Override
@@ -47,10 +55,13 @@ public class MapCommand extends AbstractCommand{
             Matcher m = getMatcher(message);
             m.find();
             if (m.group(1) == null)
-                maps = new String[]{"I", "II", "III", "IV", "V", "VI",
-                        "VII", "VIII", "IX", "X", "XI", "XII"};
+                maps = new String[]{"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X",
+                        "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII"};
             else {
                 String text = m.group(1).trim().toUpperCase()
+                        .replaceAll("17", "XVII")
+                        .replaceAll("16", "XVI").replaceAll("15", "XV")
+                        .replaceAll("14", "XIV").replaceAll("13", "XIII")
                         .replaceAll("12", "XII").replaceAll("11", "XI")
                         .replaceAll("10", "X").replaceAll("9", "IX")
                         .replaceAll("8", "VIII").replaceAll("7", "VII")
@@ -79,7 +90,7 @@ public class MapCommand extends AbstractCommand{
     @Override
     public String helpDetailed(String prefixe) {
         return help(prefixe)
-                + "\n" + prefixe + "`"  + name + "` : tire une carte du Goultarminator entre I et XII compris."
+                + "\n" + prefixe + "`"  + name + "` : tire une carte du Goultarminator entre I et XVII compris."
                 + "\n" + prefixe + "`"  + name + " `*`map1 map2 ...`* : tire une carte parmi celles spécifiées en paramètre. Nombres romains ou numériques uniquement.\n";
     }
 }
