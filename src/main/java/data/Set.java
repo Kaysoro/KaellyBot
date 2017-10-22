88eb8dcb36d032c413183990da056354e0b68725
Steven Pescheteau <steven.pescheteau@gmail.com>
2017-10-22 11:54:13 +0200

    Total bonus set can now be shown even if the list is too long for discord embedfield.

--- src/main/java/data/Set.java
+++ src/main/java/data/Set.java
@@ -23,12 +23,12 @@ public class Set implements Embedded {
     private String level;
     private String skinURL;
     private String url;
-    private String bonusTotal;
+    private List<String> bonusTotal;
     private String composition;
     private String[] bonusPano;
     private List<String> recipeTotal;
 
-    private Set(String name, String level, String bonusTotal, String skinURL, String url, String composition,
+    private Set(String name, String level, List<String> bonusTotal, String skinURL, String url, String composition,
                 String[] bonusPano, List<String> recipeTotal) {
         this.name = name;
         this.level = level;
@@ -77,7 +77,11 @@ public class Set implements Embedded {
             builder.appendField(":shopping_bags: Composition :", composition, true);
 
         if (bonusTotal != null && ! bonusTotal.isEmpty())
-            builder.appendField(":cyclone: Bonus total de la panoplie complète :", bonusTotal, true);
+            for(int i = 0; i < bonusTotal.size(); i++)
+                builder.appendField(":cyclone: Bonus total de la panoplie complète :" + (bonusTotal.size() > 1?
+                            " (" + (i + 1) + "/" + bonusTotal.size() + ")" : "") + " : ",
+                    bonusTotal.get(i), true);
+
         if (! recipeTotal.isEmpty())
             for(int i = 0; i < recipeTotal.size(); i++)
                 builder.appendField(":moneybag: liste totale d'ingrédients :" + (recipeTotal.size() > 1?
@@ -100,7 +104,7 @@ public class Set implements Embedded {
         Element element = doc.getElementsByClass("ak-encyclo-detail-illu").first().getElementsByTag("img").first();
         String skinURL = element.attr("src");
 
-        String bonusTotal = null;
+        List<String> bonusTotal = new ArrayList<>();
         String composition = null;
         String[] bonusPano = new String[doc.getElementsByClass("ak-item-list-preview").first().children().size() - 1];
         List<String> recipeTotal = new ArrayList<>();
@@ -109,7 +113,7 @@ public class Set implements Embedded {
         Elements titles = doc.getElementsByClass("ak-panel-title");
         for (Element title : titles)
             if (title.text().equals("Bonus total de la panoplie complète"))
-                bonusTotal = extractStatsFromTitle(title.parent());
+                bonusTotal = extractStatsFromTitleToList(title.parent());
             else if (title.text().equals("Bonus de la panoplie")){
                 for(int i = 0; i < bonusPano.length; i++)
                     bonusPano[i] = extractStatsFromTitle(title.parent().getElementsByClass("set-bonus-list set-bonus-" + (i + 2)).first());
@@ -163,5 +167,25 @@ public class Set implements Embedded {
         for (Element line : lines)
             tmp.append(EmojiManager.getEmojiForStat(line.text())).append(line.text()).append("\n");
         return tmp.toString();
+    }
+
+    private static List<String> extractStatsFromTitleToList(Element elem)
+    {
+        List<String> values = new ArrayList<>();
+        Elements lines = elem.getElementsByClass("ak-title");
+        StringBuilder tmp = new StringBuilder();
+        for (Element line : lines) {
+            String value = EmojiManager.getEmojiForStat(line.text()) + line.text() + "\n";
+            if (value.length() + tmp.length() > EmbedBuilder.FIELD_CONTENT_LIMIT){
+                values.add(tmp.toString());
+                tmp.setLength(0);
+            }
+            else
+                tmp.append(value);
+        }
+        if (tmp.length() > 0)
+            values.add(tmp.toString());
+
+        return values;
     }
 }
