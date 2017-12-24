3554037479b211b62358327f315f578e0111d539
Steven Pescheteau <steven.pescheteau@gmail.com>
2017-12-24 09:48:32 +0100

    Bug corrected when a guild has no message readable + correction on website503 label.

--- src/main/java/enums/Gender.java
+++ src/main/java/enums/Gender.java
@@ -1,5 +1,15 @@
 package enums;
 
 public enum Gender {
-    MALE, FEMALE;
+    MALE("mg"), FEMALE("fg"), ASEXUAL("ng");
+
+    private String definition;
+
+    Gender(String definition){
+        this.definition = definition;
+    }
+
+    public String getDefinition(){
+        return definition;
+    }
 }
