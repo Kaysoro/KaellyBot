7b93422316090f36920ca3defd2f570ef4527a36
Songfu <simon.gourdeau@hotmail.fr>
2018-12-24 11:31:31 +0100

    Statistics on guilds join time #125
    Graph generated on demand
    
    31ae6d16d13b4b550bc0c74db0e2042e3ab465a4

--- src/main/java/commands/admin/StatCommand.java
+++ src/main/java/commands/admin/StatCommand.java
@@ -2,13 +2,25 @@ package commands.admin;
 
 import commands.model.AbstractCommand;
 import enums.Language;
+import exceptions.BasicDiscordException;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.JFreeChart;
+import org.jfree.data.time.Day;
+import org.jfree.data.time.TimeSeries;
+import org.jfree.data.time.TimeSeriesCollection;
 import sx.blah.discord.handle.obj.IGuild;
 import sx.blah.discord.handle.obj.IMessage;
+import sx.blah.discord.handle.obj.IUser;
 import util.ClientConfig;
 import util.Message;
 import util.Translator;
 
-import java.util.List;
+import java.awt.image.BufferedImage;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.stream.Collectors;
 
@@ -17,10 +29,11 @@ import java.util.stream.Collectors;
  */
 public class StatCommand extends AbstractCommand {
 
-    private final static int LIMIT = 10;
+    private final static int GULD_LIMIT = 10;
+    private final static int CMD_LIMIT = 1;
 
     public StatCommand(){
-        super("stats","(\\s+-g(\\s+\\d+)?)?");
+        super("stats","(\\s+-g(\\s+\\d+)?|\\s+-cmd(\\s+\\d+)?|\\s+-hist)?");
         setAdmin(true);
     }
 
@@ -37,7 +50,7 @@ public class StatCommand extends AbstractCommand {
             Message.sendText(message.getChannel(), answer);
         }
         else if (m.group(1).matches("\\s+-g(\\s+\\d+)?")){
-            int limit = LIMIT;
+            int limit = GULD_LIMIT;
             if (m.group(2) != null) limit = Integer.parseInt(m.group(2).trim());
             StringBuilder st = new StringBuilder();
             List<IGuild> guilds = getBiggestGuilds(limit);
@@ -47,6 +60,16 @@ public class StatCommand extends AbstractCommand {
                         .append(guild.getTotalMemberCount()).append(" users\n");
             Message.sendText(message.getChannel(), st.toString());
         }
+        else if (m.group(1).matches("\\s+-cmd(\\s+\\d+)?")){
+            int limit = CMD_LIMIT;
+            if (m.group(2) != null) limit = Integer.parseInt(m.group(2).trim());
+            List<String> cmdCalled = getNumberCmdCalled(limit);
+            //TODO
+            BasicDiscordException.IN_DEVELOPPMENT.throwException(message, this, lg);
+        }
+        else if (m.group(1).matches("\\s+-hist"))
+            Message.sendImage(message.getChannel(), getJoinTimeGuildsGraph(),
+                    "stats -hist : " + Instant.now() + ".png");
     }
     /**
      *
@@ -60,6 +83,44 @@ public class StatCommand extends AbstractCommand {
                 .collect(Collectors.toList());
     }
 
+    /**
+     *
+     * @return Graphique des arrivés des guildes utilisant kaelly
+     */
+    private BufferedImage getJoinTimeGuildsGraph(){
+        IUser me = ClientConfig.DISCORD().getOurUser();
+
+        List<IGuild> guilds = ClientConfig.DISCORD().getGuilds().stream()
+                .sorted(Comparator.comparing(guild -> guild.getJoinTimeForUser(me)))
+                .collect(Collectors.toList());
+
+        TimeSeriesCollection dataSet = new TimeSeriesCollection();
+        TimeSeries series = new TimeSeries("data");
+        int guildNumber = 1;
+        for(IGuild guild : guilds)
+            series.add(new Day(Date.from(guild.getJoinTimeForUser(me))), guildNumber++);
+        dataSet.addSeries(series);
+
+        JFreeChart chart = ChartFactory.createTimeSeriesChart(
+                "Guilds join time history",
+                "Date",
+                "Discord guild number",
+                dataSet, false, false, false);
+
+        return chart.createBufferedImage(700, 500);
+    }
+
+    /**
+     *
+     * @param limit nombre de jours d'appels de commande à afficher
+     * @return Liste des appels de commandes de kaelly par jour
+     */
+    private List<String> getNumberCmdCalled(int limit){
+        List<String> result = new ArrayList<>();
+        //TODO
+        return result;
+    }
+
     @Override
     public String help(Language lg, String prefixe) {
         return "**" + prefixe + name + "** " + Translator.getLabel(lg, "stat.help");
@@ -69,6 +130,8 @@ public class StatCommand extends AbstractCommand {
     public String helpDetailed(Language lg, String prefixe) {
         return help(lg, prefixe)
                 + "\n`" + prefixe + name + "` : " + Translator.getLabel(lg, "stat.help.detailed.1")
-                + "\n`" + prefixe + name + " -g `*`n`* : " + Translator.getLabel(lg, "stat.help.detailed.2") + "\n";
+                + "\n`" + prefixe + name + " -g `*`n`* : " + Translator.getLabel(lg, "stat.help.detailed.2")
+                + "\n`" + prefixe + name + " -cmd `*`n`* : " + Translator.getLabel(lg, "stat.help.detailed.3")
+                + "\n`" + prefixe + name + " -hist` : " + Translator.getLabel(lg, "stat.help.detailed.4")+ "\n";
     }
 }
--- src/main/java/util/Message.java
+++ src/main/java/util/Message.java
@@ -8,6 +8,12 @@ import sx.blah.discord.api.internal.json.objects.EmbedObject;
 import sx.blah.discord.handle.obj.IChannel;
 import sx.blah.discord.util.*;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+
 /**
  * Created by steve on 14/11/2016.
  */
@@ -38,6 +44,37 @@ public class Message {
             return null;
         });
     }
+
+    public static void sendImage(IChannel channel, BufferedImage image, String imageName) {
+        Language lg = Translator.getLanguageFrom(channel);
+        RequestBuffer.request(() -> {
+            try {
+                ByteArrayOutputStream os = new ByteArrayOutputStream();
+                ImageIO.write(image, "png", os);
+                InputStream is = new ByteArrayInputStream(os.toByteArray());
+                new MessageBuilder(ClientConfig.DISCORD())
+                        .withChannel(channel)
+                        .withFile(is, imageName)
+                        .build();
+            } catch (RateLimitException e) {
+                LoggerFactory.getLogger(Message.class).warn(e.getMessage(), e);
+                throw e;
+            } catch (DiscordException e) {
+                Reporter.report(e, channel.isPrivate() ? null : channel.getGuild(), channel);
+                LoggerFactory.getLogger(Message.class).error(e.getMessage(), e);
+            } catch (MissingPermissionsException e) {
+                LoggerFactory.getLogger(Message.class).warn(Constants.name
+                        + " n'a pas les permissions pour appliquer cette requête.");
+                new MissingPermissionDiscordException().throwException(channel, lg, e);
+            } catch (Exception e) {
+                Reporter.report(e, channel.isPrivate() ? null : channel.getGuild(), channel);
+                LoggerFactory.getLogger(Message.class).error(e.getMessage(), e);
+            }
+            return null;
+        });
+    }
+
+
 
     public static void sendEmbed(IChannel channel, EmbedObject content){
         Language lg = Translator.getLanguageFrom(channel);
