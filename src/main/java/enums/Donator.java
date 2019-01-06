d0ca430b12741eabc0b10c2c277cf89894c66fed
Songfu <simon.gourdeau@hotmail.fr>
2019-01-06 12:28:42 +0100

    Thanks, Darkrai#6493 :)
    b291084394ebb67b786ebaa9d46c134d382cedb1

--- src/main/java/enums/Donator.java
+++ src/main/java/enums/Donator.java
@@ -2,22 +2,17 @@ package enums;
 
 public enum Donator {
 
-    IHART69("Hart69#0001", 10),
-    ELDER_MASTER("Elder-Master#7684", 20);
+    IHART69("Hart69#0001"),
+    ELDER_MASTER("Elder-Master#7684"),
+    DARKRAI("Darkrai#6493");
 
     private String name;
-    private int amount;
 
-    Donator(String name, int amount){
+    Donator(String name){
         this.name = name;
-        this.amount = amount;
     }
 
     public String getName() {
         return name;
-    }
-
-    public int getAmount() {
-        return amount;
     }
 }
