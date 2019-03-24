edc53ead8eb9ce6f92f1c06eeff9c4ca66c67178
Steven Pescheteau <steven.pescheteau@gmail.com>
2019-03-24 11:20:38 +0100

    Portal REST module added - almost done

---
+++ portal/src/test/java/com/github/kaysoro/kaellybot/portal/PortalApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.kaysoro.kaellybot.portal;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PortalApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
