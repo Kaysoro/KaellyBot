8d1510a66da15999c59ce18df1797a1eed6a901b
sgourdeau <simon.gourdeau@hotmail.fr>
2019-09-26 11:31:25 +0200

    ServerCommand adapted with this new exception (todo: job, align, whois, fetch)
     669f429f6986de5790d4e31182fa346426cb13cc

--- src/main/java/commands/config/ServerCommand.java
+++ src/main/java/commands/config/ServerCommand.java
@@ -12,10 +12,12 @@ import exceptions.NotFoundDiscordException;
 import exceptions.TooMuchDiscordException;
 import util.Message;
 import sx.blah.discord.handle.obj.IMessage;
+import util.ServerUtils;
 import util.Translator;
 
 import java.text.Normalizer;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.stream.Collectors;
@@ -25,14 +27,9 @@ import java.util.stream.Collectors;
  */
 public class ServerCommand extends AbstractCommand {
 
-    private DiscordException tooMuchServers;
-    private DiscordException notFoundServer;
-
     public ServerCommand(){
         super("server","(\\s+.+)?");
         setUsableInMP(false);
-        tooMuchServers = new TooMuchDiscordException("server");
-        notFoundServer = new NotFoundDiscordException("server");
     }
 
     @Override
@@ -45,31 +42,22 @@ public class ServerCommand extends AbstractCommand {
 
                 if (serverName.equals("-list")){
                     String sb = Translator.getLabel(lg, "server.list") + "\n" +
-                            getServersNormalized();
+                            getServerList();
                     Message.sendText(message.getChannel(), sb);
                 }
                 else if (! serverName.equals("-reset")) {
-                    serverName = Normalizer.normalize(serverName, Normalizer.Form.NFD)
-                            .replaceAll("\\p{InCombiningDiacriticalMarks}+", "")
-                            .replaceAll("\\W+", "").trim();
-                    List<ServerDofus> result = new ArrayList<>();
-                    for (ServerDofus server : ServerDofus.getServersDofus())
-                        if (Normalizer.normalize(server.getName(), Normalizer.Form.NFD)
-                                .replaceAll("\\p{InCombiningDiacriticalMarks}+", "")
-                                .replaceAll("\\W+", "").toLowerCase().trim()
-                                .startsWith(serverName))
-                            result.add(server);
+                    ServerUtils.ServerQuery query = ServerUtils.getServerDofusFromName(serverName);
 
-                    if (result.size() == 1) {
-                        guild.setServer(result.get(0));
+                    if (query.hasSucceed()) {
+                        guild.setServer(query.getServer());
                         Message.sendText(message.getChannel(), Translator.getLabel(lg, "server.request.1")
                                 .replace("{game}", Constants.game.getName())
                                 + " " + guild.getName() + " " + Translator.getLabel(lg, "server.request.2")
-                                + " " + result.get(0).getName() + ".");
-                    } else if (result.isEmpty())
-                        notFoundServer.throwException(message, this, lg);
+                                + " " + query.getServer().getName() + ".");
+                    }
                     else
-                        tooMuchServers.throwException(message, this, lg, result);
+                        query.getExceptions()
+                                .forEach(exception -> exception.throwException(message, this, lg, query.getServersFound()));
                 }
                 else {
                     guild.setServer(null);
@@ -93,33 +81,34 @@ public class ServerCommand extends AbstractCommand {
     }
 
     @Override
-    public String help(Language lg, String prefixe) {
-        return "**" + prefixe + name + "** " + Translator.getLabel(lg, "server.help")
+    public String help(Language lg, String prefix) {
+        return "**" + prefix + name + "** " + Translator.getLabel(lg, "server.help")
                 .replace("{game}", Constants.game.getName());
     }
 
     @Override
-    public String helpDetailed(Language lg, String prefixe) {
-        return help(lg, prefixe)
-                + "\n`" + prefixe + name + "` : " + Translator.getLabel(lg, "server.help.detailed.1")
+    public String helpDetailed(Language lg, String prefix) {
+        return help(lg, prefix)
+                + "\n`" + prefix + name + "` : " + Translator.getLabel(lg, "server.help.detailed.1")
                 .replace("{game}", Constants.game.getName())
-                + "\n`" + prefixe + name + " -list` : " + Translator.getLabel(lg, "server.help.detailed.2")
+                + "\n`" + prefix + name + " -list` : " + Translator.getLabel(lg, "server.help.detailed.2")
                 .replace("{game}", Constants.game.getName())
-                + "\n`" + prefixe + name + " `*`server`* : " + Translator.getLabel(lg, "server.help.detailed.3")
+                + "\n`" + prefix + name + " `*`server`* : " + Translator.getLabel(lg, "server.help.detailed.3")
                 .replace("{game}", Constants.game.getName())
-                + "\n`" + prefixe + name + " -channel `*`server`* : " + Translator.getLabel(lg, "server.help.detailed.4")
+                + "\n`" + prefix + name + " -channel `*`server`* : " + Translator.getLabel(lg, "server.help.detailed.4")
                 .replace("{game}", Constants.game.getName())
-                + "\n`" + prefixe + name + " `*`-reset`* : " + Translator.getLabel(lg, "server.help.detailed.5")
+                + "\n`" + prefix + name + " `*`-reset`* : " + Translator.getLabel(lg, "server.help.detailed.5")
                 .replace("{game}", Constants.game.getName())
-                + "\n`" + prefixe + name + " -channel -reset` : " + Translator.getLabel(lg, "server.help.detailed.6")
+                + "\n`" + prefix + name + " -channel -reset` : " + Translator.getLabel(lg, "server.help.detailed.6")
                 .replace("{game}", Constants.game.getName())
                 + "\n";
     }
 
-    private String getServersNormalized() {
+    private String getServerList() {
         StringBuilder sb = new StringBuilder("```");
         List<ServerDofus> servers = ServerDofus.getServersDofus().stream()
                 .filter(server -> server.getGame() == Game.DOFUS_TOUCH)
+                .sorted(Comparator.comparing(ServerDofus::getName))
                 .collect(Collectors.toList());
         long sizeMax = servers.stream()
                 .map(server -> server.getName().length())
