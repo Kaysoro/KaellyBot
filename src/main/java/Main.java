8dbaac9b92aec4fd0ca04a0760a0a61218ef55fe
sgourdeau <simon.gourdeau@hotmail.fr>
2019-10-03 16:59:51 +0200

    Little fix

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,7 +12,7 @@ public class Main {
     public static void main(String[] args) {
         LoggerFactory.getLogger(Main.class).info("=======================================================");
         LoggerFactory.getLogger(Main.class).info("               " + Constants.name + " v" + Constants.version
-                + " for " + Constants.game);
+                + " for " + Constants.game.getName());
         LoggerFactory.getLogger(Main.class).info("=======================================================");
       IDiscordClient client = ClientConfig.DISCORD();
       client.getDispatcher().registerListener(new ReadyListener());
--- src/main/java/enums/Game.java
+++ src/main/java/enums/Game.java
@@ -5,7 +5,7 @@ import data.Constants;
 public enum Game {
 
     DOFUS("Dofus", "https://discordapp.com/oauth2/authorize?&client_id=202916641414184960&scope=bot"),
-    DOFUS_TOUCH("Dofus Touch", Constants.invite);
+    DOFUS_TOUCH("DofusTouch", Constants.invite);
 
     private String name;
     private String botInvite;
