376e1e3a512dd57ec95a65bab3347241c1f3fbb3
Steven Pescheteau <steven.pescheteau@gmail.com>
2019-11-04 13:08:30 +0100

    ReadyListener is triggered once, even if the connexion is lost and then retrieved.

--- src/main/java/listeners/ReadyListener.java
+++ src/main/java/listeners/ReadyListener.java
@@ -28,6 +28,7 @@ public class ReadyListener {
     private GuildLeaveListener guildLeaveListener;
     private GuildUpdateListener guildUpdateListener;
     private ChannelDeleteListener channelDeleteListener;
+    private boolean isReadyOnce;
 
     public ReadyListener(){
          messageListener = new MessageListener();
@@ -35,47 +36,53 @@ public class ReadyListener {
          guildLeaveListener = new GuildLeaveListener();
          guildUpdateListener = new GuildUpdateListener();
          channelDeleteListener = new ChannelDeleteListener();
+         isReadyOnce = false;
     }
 
     public void onReady(DiscordClient client) {
-        long time = System.currentTimeMillis();
 
-        LOG.info("Ajout des différents listeners...");
-        client.getEventDispatcher().on(GuildCreateEvent.class)
-                .subscribe(guildCreateEvent -> guildCreateListener.onReady(client, guildCreateEvent));
-        client.getEventDispatcher().on(GuildDeleteEvent.class)
-                .subscribe(guildDeleteEvent -> guildLeaveListener.onReady(guildDeleteEvent));
-        client.getEventDispatcher().on(GuildUpdateEvent.class)
-                .subscribe(guildUpdateEvent -> guildUpdateListener.onReady(guildUpdateEvent));
-        client.getEventDispatcher().on(TextChannelDeleteEvent.class)
-                .subscribe(textChannelDeleteEvent -> channelDeleteListener.onReady(textChannelDeleteEvent));
+        if (!isReadyOnce) {
+            long time = System.currentTimeMillis();
 
-        LOG.info("Check des guildes...");
-        client.getGuilds().collectList().blockOptional().orElse(Collections.emptyList())
-                .forEach(guild -> {
+            LOG.info("Ajout des différents listeners...");
+            client.getEventDispatcher().on(GuildCreateEvent.class)
+                    .subscribe(guildCreateEvent -> guildCreateListener.onReady(client, guildCreateEvent));
+            client.getEventDispatcher().on(GuildDeleteEvent.class)
+                    .subscribe(guildDeleteEvent -> guildLeaveListener.onReady(guildDeleteEvent));
+            client.getEventDispatcher().on(GuildUpdateEvent.class)
+                    .subscribe(guildUpdateEvent -> guildUpdateListener.onReady(guildUpdateEvent));
+            client.getEventDispatcher().on(TextChannelDeleteEvent.class)
+                    .subscribe(textChannelDeleteEvent -> channelDeleteListener.onReady(textChannelDeleteEvent));
+
+            LOG.info("Check des guildes...");
+            client.getGuilds().collectList().blockOptional().orElse(Collections.emptyList())
+                    .forEach(guild -> {
                         if (Guild.getGuilds().containsKey(guild.getId().asString())
                                 && !guild.getName().equals(Guild.getGuild(guild).getName()))
                             Guild.getGuild(guild).setName(guild.getName());
                         else
                             client.getEventDispatcher().publish(new GuildCreateEvent(client, guild));
-                });
+                    });
 
-        // Joue à...
-        client.updatePresence(Presence.online(Activity.watching(Constants.discordInvite))).subscribe();
+            // Joue à...
+            client.updatePresence(Presence.online(Activity.watching(Constants.discordInvite))).subscribe();
 
-        LOG.info("Ecoute des flux RSS du site Dofus...");
-        RSSFinder.start();
+            LOG.info("Ecoute des flux RSS du site Dofus...");
+            RSSFinder.start();
 
-        LOG.info("Lancement du calendrier Almanax...");
-        AlmanaxCalendar.start();
+            LOG.info("Lancement du calendrier Almanax...");
+            AlmanaxCalendar.start();
 
-        LOG.info("Connexion à l'API Twitter...");
-        TwitterFinder.start();
+            LOG.info("Connexion à l'API Twitter...");
+            TwitterFinder.start();
 
-        LOG.info("Ecoute des messages...");
-        client.getEventDispatcher().on(MessageCreateEvent.class)
-                .subscribe(event -> messageListener.onReady(event));
+            LOG.info("Ecoute des messages...");
+            client.getEventDispatcher().on(MessageCreateEvent.class)
+                    .subscribe(event -> messageListener.onReady(event));
 
-        LOG.info("Mise en place des ressources en " + (System.currentTimeMillis() - time) + "ms");
+            isReadyOnce = true;
+            LOG.info("Mise en place des ressources en " + (System.currentTimeMillis() - time) + "ms");
+
+        }
     }
 }
