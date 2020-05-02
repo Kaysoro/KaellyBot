fa4ac375e642b81c74dafcda30f1a9845c8dae79
Steven Pescheteau <steven.pescheteau@gmail.com>
2020-05-02 13:26:32 +0200

    Trigger removed from the old repository

--- src/main/java/listeners/MessageListener.java
+++ src/main/java/listeners/MessageListener.java
@@ -9,8 +9,6 @@ import enums.Language;
 import exceptions.BasicDiscordException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import triggers.TriggerManager;
-import triggers.model.Trigger;
 import util.Reporter;
 import util.Translator;
 
@@ -52,10 +50,6 @@ public class MessageListener {
                                 LOG.error("onReady", e);
                             }
                         }
-
-                        TriggerManager.getTriggers().stream()
-                                .filter(trigger -> trigger.isTriggered(event.getMessage(), lg))
-                                .forEach(trigger -> trigger.execute(event.getMessage(), lg));
                     }
                 }).subscribe();
     }
