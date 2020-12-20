79a46ef526e46680565bde9ad121b656c3f314f0
Steven Pescheteau <steven.pescheteau@gmail.com>
2020-12-20 14:43:38 +0100

    [Fix] Kaelly is disconnected from discord when the first channel read is not visible (forbidden)

--- src/main/java/listeners/GuildCreateListener.java
+++ src/main/java/listeners/GuildCreateListener.java
@@ -6,11 +6,15 @@ import discord4j.common.util.Snowflake;
 import discord4j.core.DiscordClient;
 import discord4j.core.event.domain.guild.GuildCreateEvent;
 import discord4j.core.object.entity.Member;
+import discord4j.core.object.entity.Message;
+import discord4j.core.object.entity.channel.GuildMessageChannel;
 import discord4j.core.object.entity.channel.TextChannel;
 import discord4j.discordjson.json.MessageData;
+import discord4j.rest.http.client.ClientException;
 import discord4j.rest.util.Permission;
 import enums.Language;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import util.ClientConfig;
 import data.Constants;
 import data.Guild;
@@ -31,8 +35,8 @@ public class GuildCreateListener {
             if (!Guild.getGuilds().containsKey(event.getGuild().getId().asString())) {
 
                 return event.getGuild().getChannels()
-                        .filter(chan -> chan instanceof TextChannel)
-                        .map(chan -> (TextChannel) chan).take(1).flatMap(chan -> {
+                        .filter(chan -> chan instanceof GuildMessageChannel)
+                        .map(chan -> (GuildMessageChannel) chan).take(1).flatMap(chan -> {
                     Guild guild = new Guild(event.getGuild().getId().asString(), event.getGuild().getName(),
                             Translator.detectLanguage(chan));
                     guild.addToDatabase();
@@ -54,11 +58,8 @@ public class GuildCreateListener {
                                 .replaceAll("\\{owner}", owner.getMention())
                                 .replaceAll("\\{guild}", event.getGuild().getName());
 
-                        return chan.getEffectivePermissions(event.getClient().getCoreResources().getSelfId())
-                                .flatMap(perm -> perm.contains(Permission.SEND_MESSAGES) ?
-                                        chan.createMessage(customMessage) : event.getGuild().getOwner()
-                                        .flatMap(Member::getPrivateChannel)
-                                        .flatMap(ownerChan -> ownerChan.createMessage(customMessage)))
+                        return chan.createMessage(customMessage)
+                                .onErrorResume(ignored -> sendWelcomeMessageInPM(event, customMessage))
                                 .then(ClientConfig.DISCORD().getChannelById(Snowflake.of(Constants.chanReportID))
                                         .createMessage("[NEW] **" + event.getGuild().getName()
                                                 + "** (" + guild.getLanguage().getAbrev() + "), +"
@@ -71,5 +72,12 @@ public class GuildCreateListener {
             LOG.error("onReady", e);
         }
         return Flux.empty();
+    }
+
+    public Mono<Message> sendWelcomeMessageInPM(GuildCreateEvent event, String message){
+        return event.getGuild().getOwner()
+                .flatMap(Member::getPrivateChannel)
+                .flatMap(ownerChan -> ownerChan.createMessage(message)
+                        .onErrorResume(ClientException.isStatusCode(403), err -> Mono.empty()));
     }
 }
