1858c27ef69feeca17b8c811820b555a8f23c264
Steven Pescheteau <steven.pescheteau@gmail.com>
2021-06-02 19:44:30 +0200

    [FIX] Id correctly converted to String or Long

--- src/main/java/util/Translator.java
+++ src/main/java/util/Translator.java
@@ -76,10 +76,10 @@ public class Translator {
         return result;
     }
     public static Language getLanguageFrom(RestChannel channel) {
-        Guild guild = Guild.getGuilds().get(channel.getData().block().guildId().get());
+        Guild guild = Guild.getGuilds().get(channel.getData().block().guildId().get().asString());
         Language result = guild.getLanguage();
 
-        ChannelLanguage channelLanguage = ChannelLanguage.getChannelLanguages().get(Long.parseLong(String.valueOf(channel.getData().block().id())));
+        ChannelLanguage channelLanguage = ChannelLanguage.getChannelLanguages().get(channel.getData().block().id().asLong());
         if (channelLanguage != null)
             result = channelLanguage.getLang();
 
