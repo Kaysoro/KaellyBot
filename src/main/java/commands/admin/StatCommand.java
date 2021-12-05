533e4c6912ee37d7b51e3bb2df8eebe1b1bdd023
Steven Pescheteau <steven.pescheteau@gmail.com>
2021-12-05 23:07:35 +0100

    1.6.4 ready

--- src/main/java/commands/admin/StatCommand.java
+++ src/main/java/commands/admin/StatCommand.java
@@ -2,15 +2,14 @@ package commands.admin;
 
 import commands.model.AbstractCommand;
 import discord4j.common.store.action.read.ReadActions;
-import discord4j.core.DiscordClient;
 import discord4j.core.event.domain.message.MessageCreateEvent;
 import discord4j.core.object.entity.Guild;
 import discord4j.core.object.entity.Message;
 import discord4j.core.spec.MessageCreateFields;
 import discord4j.core.spec.MessageCreateSpec;
 import discord4j.discordjson.json.GuildData;
-import discord4j.discordjson.json.UserGuildData;
 import enums.Language;
+import lombok.extern.slf4j.Slf4j;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.JFreeChart;
 import org.jfree.data.time.Day;
@@ -19,7 +18,6 @@ import org.jfree.data.time.TimeSeriesCollection;
 import org.slf4j.LoggerFactory;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-import util.ClientConfig;
 import util.Reporter;
 import util.Translator;
 
@@ -37,9 +35,10 @@ import java.util.stream.Collectors;
 /**
  * Created by steve on 23/12/2017.
  */
+@Slf4j
 public class StatCommand extends AbstractCommand {
 
-    private final static int GULD_LIMIT = 10;
+    private static final int GULD_LIMIT = 10;
 
     public StatCommand(){
         super("stats","(\\s+-g(\\s+\\d+)?|\\s+-cmd(\\s+\\d+)?|\\s+-hist)?");
@@ -49,17 +48,20 @@ public class StatCommand extends AbstractCommand {
     @Override
     public void request(MessageCreateEvent event, Message message, Matcher m, Language lg) {
         if (m.group(1) == null || m.group(1).replaceAll("^\\s+", "").isEmpty()){
+            long connectedShards = event.getClient().getGatewayResources().getShardCoordinator()
+                    .getConnectedCount()
+                    .blockOptional().orElse(0);
             long totalGuild = Mono.from(event.getClient().getGatewayResources().getStore()
-                    .execute(ReadActions.countGuilds())).block();
-            long totalMembers = Mono.from(event.getClient().getGatewayResources().getStore()
-                    .execute(ReadActions.countMembers())).block();
-            long totalUsers = Mono.from(event.getClient().getGatewayResources().getStore()
-                    .execute(ReadActions.countUsers())).block();
+                    .execute(ReadActions.countGuilds()))
+                    .blockOptional().orElse(0L);
+            long totalMembers = event.getClient().getGuilds()
+                    .collect(Collectors.summingLong(Guild::getMemberCount))
+                    .blockOptional().orElse(0L);
 
             String answer = Translator.getLabel(lg, "stat.request")
+                    .replace("{shards.size}", String.valueOf(connectedShards))
                     .replace("{guilds.size}", String.valueOf(totalGuild))
-                    .replace("{members_max.size}", String.valueOf(totalMembers))
-                    .replace("{users_max.size}", String.valueOf(totalUsers));
+                    .replace("{users_max.size}", String.valueOf(totalMembers));
             message.getChannel().flatMap(chan -> chan.createMessage(answer)).subscribe();
         }
         else if (m.group(1).matches("\\s+-g(\\s+\\d+)?")){
@@ -108,6 +110,7 @@ public class StatCommand extends AbstractCommand {
                 .sort(Comparator.comparing(guild -> DateTimeFormatter.ISO_OFFSET_DATE_TIME.parse(guild.joinedAt(), Instant::from)))
                 .collectList().blockOptional().orElse(Collections.emptyList());
 
+        log.info("Computing " + guilds.size() + " guilds history");
         TimeSeriesCollection dataSet = new TimeSeriesCollection();
         TimeSeries series = new TimeSeries("data");
         int guildNumber = 1;
